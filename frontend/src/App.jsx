24c2f6484dec7cba8adb6054e593469d4a20be3a
timigyurina <timi.gyurina@gmail.com>
2022-08-26 20:39:46 +0200

    add some components, install MUI and material-icons, start working on navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import RegistrationPage from "./Pages/RegistrationPage";
+import LoginPage from "./Pages/LoginPage";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/register" element={<RegistrationPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
