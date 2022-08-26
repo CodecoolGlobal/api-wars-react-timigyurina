5855c91901bd3b034e79bad3947ed77dc884c019
timigyurina <timi.gyurina@gmail.com>
2022-08-26 13:54:00 +0200

    implement voting on a planet (for now only  with hard-coded userId)

--- backend/controllers/votingControllers.js
+++ backend/controllers/votingControllers.js
@@ -4,11 +4,56 @@ const Vote = require("../models/vote");
 const User = require("../models/user");
 
 const getAllVotes = (req, res, next) => {
-    res.json("ok");
+  res.json("ok");
 };
 
-const voteOnPlanet = (req, res, next) => {
-    res.json("ok");
+const voteOnPlanet = async (req, res, next) => {
+  //the fetchPlanetByIdFromApi mw uses the id in the params to fetch the planet from the external api
+  //const idOfPlanet = req.params.pid;
+  const planetFromApi = await req.planet.fetchedPlanet;
+  const nameOfPlanet = planetFromApi.name;
+
+  const currentTime = new Date(new Date());
+  console.log(currentTime);
+
+  const newVote = new Vote({
+    planet: nameOfPlanet,
+    date: currentTime,
+    creator: "6308a751afc38f99569cc4de", // !!!!!!!!!!!! req.userData.userId  //should get the userId from the atatched token,
+  });
+
+  //we need to check if the userId exists beacause only those can create a vote (connection between users and votes)
+  let existingUser;
+  try {
+    existingUser = await User.findById("6308a751afc38f99569cc4de"); // !!!!!!!!!!!!  req.userData.userId
+  } catch (err) {
+    const error = new Error("Creating vote failed, please try again later");
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new Error("Could not find user for the provided id");
+    error.code = 404;
+    return next(error);
+  }
+  console.log(existingUser);
+
+  try {
+    const currentSession = await mongoose.startSession();
+    currentSession.startTransaction();
+    await newVote.save({ session: currentSession }); //
+    existingUser.votes.push(newVote); //not the normal push but a spec Mongoose one that allows M to establish a connection between the 2 models
+    await existingUser.save({ session: currentSession });
+    await currentSession.commitTransaction();
+  } catch (err) {
+    const error = new Error(
+      "Creating vote failed, please try again"
+    );
+    console.log(err);
+    return next(error);
+  }
+
+  res.status(201).json({ vote: newVote });
 };
 
 module.exports = { getAllVotes, voteOnPlanet };
--- backend/routes/votingRoutes.js
+++ backend/routes/votingRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
 const votingControllers = require("../controllers/votingControllers");
+const fetchFromApi = require("../middlewares/fetchFromApi");
 const authCheck = require("../middlewares/authCheck");
 
 const router = express.Router();
 
 router.get("/planets/", votingControllers.getAllVotes);
-router.post("/planets/:pid", authCheck, votingControllers.voteOnPlanet);
+
+router.post(
+  "/planets/:pid",
+  fetchFromApi.fetchPlanetByIdFromApi,
+  /* authCheck, */ votingControllers.voteOnPlanet
+);
 
 module.exports = router;
